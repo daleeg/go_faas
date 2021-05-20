6a877dd3175c7ff2a4e8f325dd5ce9b045df2cd3
daleeg <xph_wangly10@163.com>
2021-05-20 11:24:40 +0800

    Update common.go

--- plugin/common.go
+++ plugin/common.go
@@ -1,16 +1,9 @@
-package main
-
-const (
-	GetTimeActive   = "get_time_active"   //获取时间的流程
-	DoPrintActive   = "do_print_active"   //执行打印的流程
-	PrintItemActive = "print_item_active" //执行分别打印
-)
+package util
 
 type PluginBaseInfoNode struct {
 	Name     string         // 插件名称
 	Desc     string         // 插件描述
 	Function PluginFunction // 插件可用函数
-
 }
 
 type PluginFunction struct {

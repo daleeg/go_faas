f7dc93fd2ad5e27716e3ef90a672cc3535311f94
daleeg <li.wang@cyberserval.cn>
2022-07-05 19:29:25 +0800

    [feat] 添加rsa的公钥加密和私钥解密插件函数
    Signed-off-by: daleeg <li.wang@cyberserval.cn>

--- plugin/crypt/rsaPlugin.go
+++ plugin/crypt/rsaPlugin.go
@@ -5,7 +5,6 @@ import (
 	"crypto/rsa"
 	"crypto/x509"
 	"encoding/pem"
-	"fmt"
 	"os"
 )
 
@@ -23,11 +22,11 @@ func PluginRSAGenKey(bits int, privateKeyName string, publishKeyName string) err
 	if err != nil {
 		return err
 	}
-	fmt.Println("privateKey", privateKey)
+	//fmt.Println("privateKey", privateKey)
 	//2、通过X509标准将得到的RAS私钥序列化为：ASN.1 的DER编码字符串
 
 	privateStream := x509.MarshalPKCS1PrivateKey(privateKey)
-	fmt.Println("privateStream", privateStream)
+	//fmt.Println("privateStream", privateStream)
 
 	//3、将私钥字符串设置到pem格式块中
 	privateBlock := pem.Block{
@@ -69,4 +68,50 @@ func PluginRSAGenKey(bits int, privateKeyName string, publishKeyName string) err
 	pem.Encode(fPublic, &publicBlock)
 
 	return nil
+}
+
+// PluginEncryptRSA 对数据进行加密操作
+func PluginEncryptRSA(src []byte, path string) (res []byte, err error) {
+	//1.获取秘钥（从本地磁盘读取）
+	f, err := os.Open(path)
+	if err != nil {
+		return
+	}
+	defer f.Close()
+	fileInfo, _ := f.Stat()
+
+	b := make([]byte, fileInfo.Size())
+	f.Read(b)
+
+	// 2、将得到的字符串解码
+	block, _ := pem.Decode(b)
+	// 使用X509将解码之后的数据 解析出来
+	//x509.MarshalPKCS1PublicKey(block):解析之后无法用，所以采用以下方法：ParsePKIXPublicKey
+
+	keyInit, err := x509.ParsePKIXPublicKey(block.Bytes) //对应于生成秘钥的x509.MarshalPKIXPublicKey(&publicKey)
+	//keyInit1,err:=x509.ParsePKCS1PublicKey(block.Bytes)
+	if err != nil {
+		return
+	}
+	//4.使用公钥加密数据
+	pubKey := keyInit.(*rsa.PublicKey)
+	res, err = rsa.EncryptPKCS1v15(rand.Reader, pubKey, src)
+	return
+}
+
+// PluginDecryptRSA 对数据进行解密操作
+func PluginDecryptRSA(src []byte, path string) (res []byte, err error) {
+	//1.获取秘钥（从本地磁盘读取）
+	f, err := os.Open(path)
+	if err != nil {
+		return
+	}
+	defer f.Close()
+	fileInfo, _ := f.Stat()
+	b := make([]byte, fileInfo.Size())
+	f.Read(b)
+	block, _ := pem.Decode(b)                                 //解码
+	privateKey, err := x509.ParsePKCS1PrivateKey(block.Bytes) //还原数据
+	res, err = rsa.DecryptPKCS1v15(rand.Reader, privateKey, src)
+	return
 }
